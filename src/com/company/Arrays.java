a2a3a0f0e40c4a1692096b428707cdc484097043
ni.guliyev <nicatguliyev3@gmail.com>
2021-12-06 21:58:59 +0400

    Multidimesnsial Arrays

---
+++ src/com/company/Arrays.java
@@ -0,0 +1,48 @@
+package com.company;
+
+public class Arrays {
+    public static void main(String[] args) {
+//       int[] arr  = {1, 3 , -1, 4, 0, -5, 2};
+//       for(int i = 0; i<arr.length; i++){
+//           int min = arr[i];
+//           for(int j = i; j < arr.length; j++){
+//               if(arr[j]<=min){
+//                   min = arr[j];
+//                   arr[j] = arr[i];
+//                   arr[i] = min;
+//               }
+//           }
+//       }
+//       for (int k = 0; k<arr.length; k++){
+//           System.out.println(arr[k]);
+//       }
+
+//        int[][] x = {{2, -4}, {3, 5 -9}, {-12, 1, 4, 0}};
+//         for(int i = 0; i <x.length; i ++){
+//             int[] arr = x[i];
+//             for(int j = 0; j < arr.length; j++){
+//                 int min = arr[j];
+//                 for(int k = j; k < arr.length; k++){
+//                     if(arr[k] <= min){
+//                         min = arr[k];
+//                         arr[k] = arr[j];
+//                         arr[j] = min;
+//                     }
+//
+//                 }
+//             }
+//             x[i] = arr;
+//         }
+//
+//         for(int i = 0; i < x.length; i++){
+//             String z = "{";
+//             for(int j = 0; j< x[i].length; j++){
+//                 z += x[i][j] + " ";
+//             }
+//             z+= "}";
+//             System.out.println(z);
+//         }
+//    }
+//}
+    }
+}
