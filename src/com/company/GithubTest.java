5df5b8b0bd81222d91a48ff1584c4b0bdc4a3ad4
ni.guliyev <nicatguliyev3@gmail.com>
2021-12-09 17:37:53 +0400

    Problem Solved

--- src/com/company/GithubTest.java
+++ src/com/company/GithubTest.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class GithubTest {
     public static void main(String[] args) {
-        System.out.println("Salamm");
+        System.out.println("Salammmm");
     }
 }
